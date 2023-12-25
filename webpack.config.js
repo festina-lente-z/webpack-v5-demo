e43263585c89b3e62748741d1e287426b8e4da71
BellaZZZZZ <bellazhao_321@qq.com>
2023-12-25 23:59:45 +0800

    ✨ feat(*):管理输出

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: { index: "./src/index.js", print: "./src/print.js" },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出'
+    }),
+    new WebpackManifestPlugin()
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
   },
-}
+};

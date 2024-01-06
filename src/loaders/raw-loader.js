b46e167b5c4c4ef4485b41b43c4960aad93b7600
festina-lente-z <bellazhao_321@qq.com>
2024-01-06 22:46:40 +0800

    ✨ feat(*):loader获取参数；异常处理

--- src/loaders/raw-loader.js
+++ src/loaders/raw-loader.js
@@ -1,6 +1,19 @@
+const loaderUtils = require('loader-utils');
+
 // 将一个文件转化为 string
 module.exports = function (source) {
+    const { name } = loaderUtils.getOptions(this);
+    console.log('name', name)
     // ES6 模板字符串为什么存在安全问题？
     const json = JSON.stringify(source).replace(/\u2028/g, '\\u2028').replace(/\u2029/g, '\\u2029');
-    return `export default ${json}`;
+
+    // 方式一
+    // throw new Error('Error');
+    // return `export default ${json}`;
+
+    // 方式二
+    // this.callback(new Error('Error 1'), json, 1, 2, 3);
+
+    // 回传多个值
+    this.callback(null, json, 1, 2, 3);
 }
--- src/run-loader.js
+++ src/run-loader.js
@@ -2,11 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const { runLoaders } = require('loader-runner');
 runLoaders({
-    resource: "./demo.txt",
-    loaders: [
-        path.resolve(__dirname, './loaders/raw-loader'),
-    ],
-    readResource: fs.readFile.bind(fs)
+  resource: "./demo.txt",
+  loaders: [
+    {
+      loader: path.resolve(__dirname, './loaders/raw-loader'),
+      options: {
+        name: 'Alice'
+      }
+    },
+  ],
+  readResource: fs.readFile.bind(fs)
 }, (err, result) => {
-    err ? console.log(err) : console.log(result);
+  err ? console.log(err) : console.log(result);
 });
